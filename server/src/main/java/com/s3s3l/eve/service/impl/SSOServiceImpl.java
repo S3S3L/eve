845f6ac0ccabc61dfc4dc1f9f74f02228660fccb
kehewei <kehewei@hellobike.com>
2018-06-28 13:35:32 +0800

    change mail

--- server/src/main/java/com/s3s3l/eve/service/impl/SSOServiceImpl.java
+++ server/src/main/java/com/s3s3l/eve/service/impl/SSOServiceImpl.java
@@ -3,7 +3,7 @@
  * File Name:SSOServiceImpl.java 
  * Package Name:com.s3s3l.eve.service.impl 
  * Date:Sep 6, 20174:49:21 PM 
- * Copyright (c) 2017, kehewei@hellobike.com All Rights Reserved. 
+ * Copyright (c) 2017, kehw.zwei@gmail.com All Rights Reserved. 
  * 
 */
 
